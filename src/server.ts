6c8a552c891273b963da6466e531d1b400baad7e
hasibulh798 <hasibulh798@example.com>
2026-06-05 00:39:53 +0600

    modify server.ts file

--- src/server.ts
+++ src/server.ts
@@ -17,7 +17,7 @@ app.use(express.json());
 initDB();
 
 //Test route
-app.get("/api/v1", (req: Request, res: Response) => {
+app.get("/", (req: Request, res: Response) => {
   res.status(200).json({
     message: "Welcome to the Vehicle Rental System API",
   });
@@ -38,3 +38,6 @@ app.use("/api/v1/bookings", bookingRoutes);
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
+export default app;
+
